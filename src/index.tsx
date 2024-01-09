66ec6c4ba517ec8b4507d3c456efd01f7be7351c
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-01-10 00:32:17 +0900

    feat: shorten and restore url

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import { Hono } from 'hono';
-const app = new Hono();
+import { customAlphabet } from 'nanoid';
+
+interface Bindings {
+	URL_BINDING: KVNamespace;
+}
+
+const app = new Hono<{ Bindings: Bindings }>();
+const nanoid = customAlphabet('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz', 8);
+
 app.get('/', (c) => {
 	return c.html(
 		<html>
@@ -13,6 +21,37 @@ app.get('/', (c) => {
 			</form>
 		</html>
 	);
+});
+
+app.post('/api/links', async (c) => {
+	const body = await c.req.parseBody();
+	const url = body['url'];
+
+	if (!url || typeof url != 'string') {
+		return c.text('Missing URL', 400);
+	}
+
+	const key = nanoid();
+	await c.env.URL_BINDING.put(key, url);
+
+	return c.html(
+		<html>
+			<h1>clm</h1>
+			<p>{`${new URL(c.req.url).origin}/${key}`}</p>
+			<p>{url}</p>
+		</html>
+	);
+});
+
+app.get('/:key', async (c) => {
+	const key = c.req.param('key');
+	const url = await c.env.URL_BINDING.get(key);
+
+	if (url == null) {
+		return c.notFound();
+	}
+
+	return c.redirect(url);
 });
 
 export default app;
