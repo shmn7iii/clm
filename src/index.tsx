b262f82506c17a2d1fd749373f70e691cf69f706
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-01-10 13:24:57 +0900

    style: introduce tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { html } from 'hono/html';
 import { customAlphabet } from 'nanoid';
 
 interface Bindings {
@@ -8,18 +9,50 @@ interface Bindings {
 const app = new Hono<{ Bindings: Bindings }>();
 const nanoid = customAlphabet('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz', 8);
 
+const Layout = (props: { children?: any }) => html`<!DOCTYPE html>
+	<html>
+		<head>
+			<title>clm</title>
+			<script src="https://cdn.tailwindcss.com"></script>
+		</head>
+		<body class="h-screen w-screen px-44 py-6">
+			<a href="/" class="absolute">
+				<h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+					<span class="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">clm</span>
+					<small class="ms-2 text-2xl font-semibold"> URL Shortener</small>
+				</h1>
+			</a>
+			<div class="h-full w-full flex items-center">${props.children}</div>
+		</body>
+	</html>`;
+
 app.get('/', (c) => {
 	return c.html(
-		<html>
-			<h1>clm</h1>
-			<form action="/api/links" method="post">
-				<div>
-					<label>URL</label>
-					<input type="url" name="url" placeholder="URL" required />
-					<button>Submit</button>
-				</div>
-			</form>
-		</html>
+		<Layout>
+			<div class="w-full">
+				<form action="/api/links" method="post">
+					<label for="search" class="block mb-2 text-xl font-bold text-gray-900">
+						Let's shorten the URL!
+					</label>
+					<div class="relative">
+						<input
+							type="url"
+							name="url"
+							id="url"
+							class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+							placeholder="URL"
+							required
+						/>
+						<button
+							type="submit"
+							class="text-white absolute end-2.5 bottom-2.5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+						>
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		</Layout>
 	);
 });
 
@@ -34,12 +67,27 @@ app.post('/api/links', async (c) => {
 	const key = nanoid();
 	await c.env.URL_BINDING.put(key, url);
 
+	const shortened = `${new URL(c.req.url).origin}/${key}`;
+
 	return c.html(
-		<html>
-			<h1>clm</h1>
-			<p>{`${new URL(c.req.url).origin}/${key}`}</p>
-			<p>{url}</p>
-		</html>
+		<Layout>
+			<div>
+				<h1 class="mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+					<span
+						class="h-2 bg-no-repeat bg-bottom bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+						style="background-size: 100% 15%;"
+					>
+						<a href={shortened}>{shortened}</a>
+					</span>
+				</h1>
+				<p class="text-xl font-bold">
+					was issued as a shortened URL to{' '}
+					<a href={url} class="font-medium text-blue-600 hover:underline">
+						{url}
+					</a>
+				</p>
+			</div>
+		</Layout>
 	);
 });
 
