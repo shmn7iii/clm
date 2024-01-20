e3d77197e4ff1aebea1bb10beda5c1f8d0b834ec
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-01-20 13:56:54 +0900

    style: change title font to 'Fugaz One'

--- src/index.tsx
+++ src/index.tsx
@@ -14,12 +14,15 @@ const Layout = (props: { children?: any }) => html`<!DOCTYPE html>
 		<head>
 			<title>clm</title>
 			<script src="https://cdn.tailwindcss.com"></script>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+			<link href="https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap" rel="stylesheet" />
 		</head>
 		<body class="h-screen w-screen px-44 py-6">
 			<a href="/" class="absolute">
-				<h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+				<h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl" style="font-family: 'Fugaz One';">
 					<span class="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">clm</span>
-					<small class="ms-2 text-2xl font-semibold"> URL Shortener</small>
+					<small class="ms-2 text-2xl font-sans font-bold"> URL Shortener</small>
 				</h1>
 			</a>
 			<div class="h-full w-full flex items-center">${props.children}</div>
